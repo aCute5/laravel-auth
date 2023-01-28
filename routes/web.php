8314fd348954cf4a72f0c16b48dde5f586eadef4
aCute5 <costantini95dario@gmail.com>
2023-01-28 09:35:53 +0100

    try to adding crud

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Database\Schema\PostgresBuilder;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,17 +17,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'Admin\HomeController@index')->name('admin.home');
 
 Route::middleware('auth')
     ->namespace('Admin')
     ->name('admin.')
     ->prefix('admin')
     ->group(function () {
-        Route::get('/', 'PageController@dashbord')->name('dashboard');
+        Route::get('/', 'PageController@dashboard')->name('dashboard');
         Route::resource('posts', 'PostController');
     });
